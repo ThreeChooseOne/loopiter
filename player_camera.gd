35140cbd37640af137c88e52cea121acd8ba4cfd
clicksilver <generho@gmail.com>
2025-08-01 19:08:30 -0700

    add player camera

---
+++ player_camera.gd
@@ -0,0 +1,40 @@
+extends Camera2D
+
+var max_viewport_size: Vector2
+
+func _ready() -> void:
+	max_viewport_size = get_viewport_rect().size
+	update_camera_position(Vector2(0,0))
+
+func _input(event: InputEvent):
+	handle_movement_input(event)
+	pass
+
+func handle_movement_input(event: InputEvent) -> bool:
+	if event is not InputEventKey:
+		return false
+	var movement_step: float = 50.0
+	
+	if event.pressed and event.keycode == KEY_UP:
+		update_camera_position(movement_step * Vector2(0, -1))
+		return true
+	if event.pressed and event.keycode == KEY_DOWN:
+		update_camera_position(movement_step * Vector2(0, 1))
+		return true
+	if event.pressed and event.keycode == KEY_LEFT:
+		update_camera_position(movement_step * Vector2(-1, 0))
+		return true
+	if event.pressed and event.keycode == KEY_RIGHT:
+		update_camera_position(movement_step * Vector2(1, 0))
+		return true
+
+	return false
+	
+func update_camera_position(delta: Vector2):
+	var new_pos = position + delta
+	new_pos = new_pos.max(Vector2(0,0)+get_camera_size()/2)
+	new_pos = new_pos.min(max_viewport_size-get_camera_size()/2)
+	position = new_pos
+
+func get_camera_size() -> Vector2:
+	return max_viewport_size/zoom
