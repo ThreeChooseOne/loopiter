c530c7ab9bd735c603cda4a40780cc5ef1be3304
clicksilver <62039509+clicksilver@users.noreply.github.com>
2025-08-01 20:41:32 -0700

    visualize orbits and cleanup camera (#8)
    * cleanup camera init
    
    * add debug viz for orbits
    
    * make camera zoomable with scroll

--- main.gd
+++ main.gd
@@ -6,15 +6,22 @@ var orbits: Array[BaseOrbit] = []
 
 var moons: Array[OrbitingBody] = []
 
+var max_viewport_size: Vector2
+
 func _ready() -> void:
+	max_viewport_size = get_viewport_rect().size
+	
 	# Initialize orbit with a random planet
 	orbit.add_random_planet()
 	
 	
 	# Add 5 orbits
-	for n in 5:
+	var min_orbit_radius = 200
+	var max_orbit_radius = 300
+	
+	for n in 21:
 		# TODO: figure out the radius ranges
-		var orbit_radius = (n+1)*300.0
+		var orbit_radius = min_orbit_radius + (n/21.0)*(max_orbit_radius - min_orbit_radius)
 		var center = get_viewport_rect().size/2.0
 		var new_orbit = BaseOrbit.new(orbit_radius, center)
 		orbits.append(new_orbit)
@@ -24,15 +31,14 @@ func _ready() -> void:
 	moons.append(gen_moon)
 	orbits[0].add_child(gen_moon.get_follower())
 	
+	orbits[0].visualize_debug(true)
+	orbits[10].visualize_debug(true)
+	orbits[20].visualize_debug(true)
+
 	# Hooks up signal to increment research meter
 	$PlayerCamera.enter_key_pressed.connect(gen_moon._on_enter_pressed)
-	
-	# TODO: Add more moons
-	
-	# TODO: Refactor this visualization code somewhere else
-	# debug_orbit = Line2D.new()
-	# debug_orbit.points = orbits[0].curve.get_baked_points()
-	# add_child(debug_orbit)
+
+	$PlayerCamera.update_camera_position(Vector2(0,0))
 	
 func _process(delta: float) -> void:
 	# Should main be updating the orbits?
