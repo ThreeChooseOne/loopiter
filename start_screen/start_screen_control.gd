29e69d779d0143c7c6db607ce95d4de6bbf42d74
Russell Cullen <russellcullen@nypl.org>
2025-08-03 12:06:32 -0400

    Moon on start screen

--- start_screen/start_screen_control.gd
+++ start_screen/start_screen_control.gd
@@ -3,8 +3,16 @@ extends Node2D
 signal start_button_pressed
 signal tutorial_button_pressed
 
+const speed = 0.025
+
+func _ready() -> void:
+	%Moon.progress_ratio = randf()
+
 func _on_start_game_pressed() -> void:
 	start_button_pressed.emit()
 
 func _on_tutorial_pressed() -> void:
 	tutorial_button_pressed.emit()
+
+func _process(delta: float) -> void:
+	%Moon.progress_ratio += speed * delta
